9bdb20db57bf20a89698ac62e9d84d877ee7de02
mtnvdsk <mtnvdsk@gmail.com>
2024-02-05 17:48:30 +0530

    Arrays

---
+++ Arrays/Rearrange Array Alternately.java
@@ -0,0 +1,17 @@
+public class Solution 
+{ 
+    static void rearrange(int[] arr, int n) 
+    { 
+        int temp[] = arr.clone();
+        int small = 0, large = n - 1; 
+        boolean flag = true; 
+        for (int i = 0; i < n; i++) { 
+            if (flag) 
+                arr[i] = temp[large--]; 
+            else
+                arr[i] = temp[small++]; 
+  
+            flag = !flag; 
+        } 
+    }
+}
