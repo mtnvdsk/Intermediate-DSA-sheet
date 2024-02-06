b1b92b28baaee3167b691630845428c2ce304440
mtnvdsk <mtnvdsk@gmail.com>
2024-02-06 21:11:32 +0530

    Arrays

---
+++ Arrays/two pointer.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class Solution {
+	public static void main(String[] args)
+	{
+		int arr[] = { 2, 3, 5, 8, 9, 10, 11 };
+
+		int val = 17;
+
+		System.out.println(isPairSum(arr, arr.length, val));
+	}
+	private static int isPairSum(int A[], int N, int X)
+	{
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (i == j)
+					continue;
+				if (A[i] + A[j] == X)
+					return 1;
+				if (A[i] + A[j] > X)
+					break;
+			}
+		}
+		return 0;
+	}
+}
