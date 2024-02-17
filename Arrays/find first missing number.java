2f172a712e133f28714d05bc639b6c41bed6f409
mtnvdsk <mtnvdsk@gmail.com>
2024-02-17 19:28:04 +0530

    Arrays

---
+++ Arrays/find first missing number.java
@@ -0,0 +1,21 @@
+  
+import java.io.*; 
+  
+class Solution 
+{ 
+    int findFirstMissing(int array[], int start, int end)  
+    { 
+        if (start > end) 
+            return end + 1; 
+  
+        if (start != array[start]) 
+            return start; 
+  
+        int mid = (start + end) / 2; 
+
+        if (array[mid] == mid) 
+            return findFirstMissing(array, mid+1, end); 
+  
+        return findFirstMissing(array, start, mid); 
+    }
+}
