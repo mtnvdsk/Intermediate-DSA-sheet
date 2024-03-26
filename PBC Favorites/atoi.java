91adcceee587414bb9283b6b41f0e7d60cae2cd3
mtnvdsk <mtnvdsk@gmail.com>
2024-03-26 21:29:02 +0530

    PBC Favoriate

---
+++ PBC Favorites/atoi.java
@@ -0,0 +1,28 @@
+class Solution
+{
+    int atoi(String s) {
+        int idx = 0;
+        if(s.charAt(0) == '-'){
+           idx = 1;
+        }
+        int num = 0;
+        for( int i = idx; i<s.length(); i++){
+            if(s.charAt(i)-0 >= 48 && s.charAt(i)-0 <= 57 ){
+                num = num*10 + (s.charAt(i)-48);
+                
+            }else{
+               num = -1;
+                break;
+            }
+            
+            
+        }
+        
+        if( idx == 1 && num != -1){
+             return -1*num;
+        }
+        return num;
+        
+
+    }
+}
